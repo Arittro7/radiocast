b8584e9636df7d86f0bcbc940bca0b12db1c2c9c
Arittro7 <arittrowpdev@gmail.com>
2025-06-11 01:25:30 +0600

    convex inistall

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
+"use client";
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
-import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-2xl font-bold">Trending Podcast</h1>
+
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </div>
+
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {podcastData.map(({ id, title, description, imgURL }) => (
             <PodcastCard
