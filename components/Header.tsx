2edbd28f5136ac33f27203418fd131009641d3b1
Arittro7 <arittrowpdev@gmail.com>
2025-06-14 00:21:27 +0600

    podcast generate

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,9 @@ const Header = ({ headerTitle, titleClassName}: { headerTitle?: string; titleCla
   return (
     <header className="flex items-center justify-between">
       {headerTitle ? (
-        <h1 className={cn('text-18 font-bold text-white-1', titleClassName)}>{headerTitle}</h1>
+        <h1 className={cn(' font-bold', titleClassName)}>{headerTitle}</h1>
       ): <div />}
-      <Link href="/discover" className="text-16 font-semibold text-orange-1">
+      <Link href="/discover" className=" font-semibold text-orange-400">
         See all
       </Link>
     </header>
